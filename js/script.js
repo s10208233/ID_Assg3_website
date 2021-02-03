751b875abcb0e44d5fae0a339753e60965850f17
Keng Shion <s10204765@connect.np.edu.sg>
2021-02-04 04:39:35 +0800

    added more conditions for deckofcards to work better but still have some bugs

--- js/script.js
+++ js/script.js
@@ -7,8 +7,22 @@ function openAPI(buttonId){
     $("#api-control-nasa").hide();
     console.log(buttonId)
     if (buttonId == "api-cards"){
-        $('#api-content').empty();
+        $("#api-content").empty();
+        $("#api-content").append('<lottie-player src="https://assets4.lottiefiles.com/packages/lf20_9ngjlC.json" id="winner" background="transparent" speed="1"  style="width: 300px; height: 300px; margin: auto;"  loop  autoplay></lottie-player>');
+        $("#winner").hide();
+        $("#api-content").append('<lottie-player src="https://assets6.lottiefiles.com/packages/lf20_YVvJHa.json" id="loser" background="transparent"  speed="1"  style="width: 300px; height: 300px; margin: auto;"  loop  autoplay></lottie-player>');
+        $("#loser").hide();
+        $("#api-content").append('<lottie-player src="https://assets6.lottiefiles.com/packages/lf20_yriifcob.json" id="tie" background="transparent"  speed="1"  style="width: 300px; height: 300px; margin: auto;"  loop  autoplay></lottie-player>');
+        $("#tie").hide();
+        $("#api-content").append('<lottie-player src="https://assets6.lottiefiles.com/packages/lf20_ho8hW5.json" id="error" background="transparent"  speed="1"  style="width: 300px; height: 300px; margin: auto;"  loop  autoplay></lottie-player>');
+        $("#error").hide();
+        $("#draw").hide();
+        $("#skip").hide();
+        $("#deckbtn").show();
+        $("#api-content").append('<div id="playerHand"></div>');
+        $("#api-content").append('<div id="botHand"></div>');   
         $("#api-control-cards").show();
+        
     }
     else if (buttonId == "api-nasa"){
         $('#api-content').empty();
