80b94e0e791d4524aa19d9a98ab33b773159db6f
Lester Cheong <s10208233@connect.np.edu.sg>
2021-02-03 11:37:45 +0800

    made api content modal bigger and started created nasa js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,13 @@
 
 function test(buttonId){
     $("#api-control-cards").hide();
+    $("#api-control-nasa").hide();
     console.log(buttonId)
     if (buttonId == "api-cards"){
         $("#api-control-cards").show();
     }
+    if (buttonId == "api-nasa"){
+        $("#api-control-nasa").show();
+    }
+    
 }
