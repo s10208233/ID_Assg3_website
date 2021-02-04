06f9cf6d8c6b4a448201439a04e589e293b79c65
Lester Cheong <s10208233@connect.np.edu.sg>
2021-02-04 13:02:03 +0800

    started trivia js

--- js/script.js
+++ js/script.js
@@ -5,14 +5,19 @@
 function openAPI(buttonId){
     $("#api-control-cards").hide();
     $("#api-control-nasa").hide();
+    $("#api-control-trivia").hide();
     console.log(buttonId)
     if (buttonId == "api-cards"){
         $('#api-content').empty();
         $("#api-control-cards").show();
     }
-    else if (buttonId == "api-nasa"){
+    if (buttonId == "api-nasa"){
         $('#api-content').empty();
         $("#api-control-nasa").show();
+    }
+    if (buttonId == "api-trivia"){
+        $("#api-content").empty();
+        $("#api-control-trivia").show();
     }
     
 
